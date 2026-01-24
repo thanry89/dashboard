6ebcdd9593711fd0eb5b83a94a10d583c3c3d901
jhzambrano <thanry89@gmail.com>
2026-01-23 20:25:39 -0500

    Inicio

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+alarm_page = st.Page('paginas/alarm.py', title='Alarmas')
+#iub_page = st.Page('paginas/iub.py', title='IPPATH')
+#cell_page = st.Page('paginas/cells.py', title='Celdas')
+#energia_page = st.Page('paginas/energia.py', title='Energia')
+#mona_page = st.Page('paginas/mona.py', title='Mona_KPI')
+
+#pg = st.navigation([alarm_page, iub_page, cell_page, energia_page, mona_page])
+pg = st.navigation([alarm_page])
+
+# Page configuration
+st.set_page_config(
+    page_title="O&M RI",
+    layout="wide",
+    initial_sidebar_state="expanded")
+pg.run()
