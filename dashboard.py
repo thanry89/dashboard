0fdbe3973ae6edafb1510979ade119973f337831
JonathanZ <thanry89@gmail.com>
2026-01-24 14:04:50 -0500

    Incluir Pagina Cells 2

--- dashboard.py
+++ dashboard.py
@@ -2,12 +2,11 @@ import streamlit as st
 
 alarm_page = st.Page('paginas/alarm.py', title='Alarmas')
 #iub_page = st.Page('paginas/iub.py', title='IPPATH')
-#cell_page = st.Page('paginas/cells.py', title='Celdas')
+cell_page = st.Page('paginas/cells.py', title='Celdas')
 #energia_page = st.Page('paginas/energia.py', title='Energia')
 #mona_page = st.Page('paginas/mona.py', title='Mona_KPI')
 
-#pg = st.navigation([alarm_page, iub_page, cell_page, energia_page, mona_page])
-pg = st.navigation([alarm_page])
+pg = st.navigation([alarm_page, cell_page])
 
 # Page configuration
 st.set_page_config(
